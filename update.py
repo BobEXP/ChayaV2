93e30b93a615ff21f868314a25a7b7bca89e1db6
BobEXP <100017604+BobEXP@users.noreply.github.com>
2022-10-15 13:01:00 +0400

    Update update.py
    Simplify script

--- update.py
+++ update.py
@@ -1,19 +1,14 @@
 import asyncio
-from core.utils import current_runtime
-
-# Function > Download Updated Archive
-async def download_update():
-	runtime = current_runtime()
-	url = f"https://github.com/BobEXP/ChayaV2/archive/refs/heads/{runtime}.zip"
-	await download_file(url, "req")
-
-	status(2, "Please replace the current archive with downloaded archive.")
-	status(0, "Download Complete!")
+from core.utils import *
 
 
 # Function > Initialize
 async def init() -> None:
-	await download_update()
+	runtime = current_runtime()
+	url = f"https://github.com/BobEXP/ChayaV2/archive/refs/heads/{runtime}.zip"
+	await download_file(url, "req")
+	status(2, "Please replace the current archive with downloaded archive.")
+
 
 # Main
 if __name__ == "__main__":
